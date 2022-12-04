34e55d7bf26bcde01d4da23c38c1442cc72814f9
Artem Shpynov <artem.shpynov@security-gen.com>
2022-12-04 22:56:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DIR=$(pwd)
 cd $(dirname $BASH_SOURCE)
 
 echo "Copy package to /userdata/share..."
-find ./ -maxdepth 1 -mindepth 1 -type d ! -name patches | xargs -t -I {} cp -rf {} /
+find ./megadrive_restyle/ -maxdepth 1 -mindepth 1 -type d ! -name .git | xargs -t -I {} cp -rf {} /
 . /userdata/share/megadrive_restyle/install.sh
 
 cd $DIR
